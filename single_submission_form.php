a403641f54f8e77dbc38a47ec77afe605cb2ab7b
trevor-c <trevor.cunningham@uregina.ca>
2016-07-28 14:14:47 -0600

    fixing lang string call to point to right location (assign, not assignment)

--- single_submission_form.php
+++ single_submission_form.php
@@ -183,7 +183,7 @@ class kalvidassign_singlesubmission_form extends moodleform {
 
         }
 
-        $mform->addElement('static', 'finalgrade', get_string('currentgrade', 'assignment').':' ,$grade);
+        $mform->addElement('static', 'finalgrade', get_string('currentgrade', 'assign').':' ,$grade);
         $mform->setType('finalgrade', PARAM_INT);
 
         /* Feedback section */
